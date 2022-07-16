15f9e9755ca527bfa84e5df7e3bec1e7fa34a722
madgear <anthony.monastrial@gmail.com>
2022-07-16 10:07:25 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
 <?php
 
 ?>
+
+<script type="importmap">
+  {
+    "imports": {
+      "vue": "https://unpkg.com/vue@3/dist/vue.esm-browser.js"
+    }
+  }
+</script>
+
+<div id="app">{{ message }}</div>
+
+<script type="module">
+  import { createApp } from 'vue'
+
+  createApp({
+    data() {
+      return {
+        message: 'Hello Vue!'
+      }
+    }
+  }).mount('#app')
+</script>
